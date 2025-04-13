4e0866f5096ad09990d4b86b68f4d7aec52660c0
Khushbu Patil <80099800+PatilKhushbu@users.noreply.github.com>
2025-04-13 12:02:12 +0200

    Add files via upload

---
+++ HITS/hits.cpp
@@ -0,0 +1,202 @@
+
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include <iomanip>
+
+
+#include <limits>
+typedef std::numeric_limits< double > dbl;
+
+
+#include <GTL/GTL.h>
+#include <GTL/components.h>
+
+#include "mygraph.h"
+
+
+
+int main (int argc, const char * argv[]) 
+{
+	if (argc < 2)
+	{
+		cout << "Usage: hits <file-name>" << endl;
+		exit(1);
+	}
+	char filename[256];
+	strcpy (filename, argv[1]);
+
+  	// ---------------------------------------------------------	
+  	// Read graph	
+
+ 	MyGraph G;
+	
+	GML_error err  = G.load (filename);
+	if (err.err_num != GML_OK)
+	{
+		cerr << "Error (" << err.err_num << ") loading graph from file \"" << filename << "\"";
+		switch (err.err_num)
+		{
+			case GML_FILE_NOT_FOUND: cerr << "A file with that name doesn't exist."; break;
+			case GML_TOO_MANY_BRACKETS: cerr << "A mismatch of brackets was detected, i.e. there were too many closing brackets (])."; break;
+			case GML_OPEN_BRACKET: cerr << "Now, there were too many opening brackets ([)";  break;
+			case GML_TOO_MANY_DIGITS: cerr << "The number of digits a integer or floating point value can have is limited to 1024, this should be enough :-)";  break;
+			case GML_PREMATURE_EOF: cerr << "An EOF occured, where it wasn't expected, e.g. while scanning a string."; break;
+			case GML_SYNTAX: cerr << "The file isn't a valid GML file, e.g. a mismatch in the key-value pairs."; break;
+			case GML_UNEXPECTED: cerr << "A character occured, where it makes no sense, e.g. non-numerical characters"; break;
+			case GML_OK: break;
+		}
+		cerr << endl;
+		exit(1);
+	}
+	else
+	{
+		cout << "Graph read from file \"" << filename << "\" has " << G.number_of_nodes() << " nodes and " << G.number_of_edges() << " edges" << endl;
+	}
+	
+	
+	if (1)
+	{
+		// Page rank
+		node_map<double> page_rank (G, 0.0);
+		double damp = 0.85;
+		double norm = 1 - damp;
+		
+		int k = 40;
+		for (int i = 0; i < k; i++)
+		{
+			node p;
+			forall_nodes (p, G)
+			{					
+				double vote = 0.0;
+				
+				// Each incoming edge has a "vote"
+				node::in_edges_iterator it = p.in_edges_begin();
+				node::in_edges_iterator end = p.in_edges_end();
+				while (it != end)
+				{
+					node q = p.opposite(*it);
+					
+					// q has an edge to p, and a vote
+					// weighted by the number of edges leaving q
+					vote += page_rank[q] / (double)q.outdeg();
+					it++;
+				}
+				
+				page_rank[p] = norm + damp * vote;
+			}
+		}		
+		
+		// Results
+		cout << "Node\tPage rank" << endl;
+		node n;
+		forall_nodes (n, G)
+		{
+			cout << G.get_node_label(n) << "\t" << std::fixed << std::setprecision(3) << page_rank[n]  << endl;
+		}
+
+	}
+	else
+	{
+		// Hits
+	
+		node_map<double> authority_weight (G, 1.0);
+		node_map<double> hub_weight (G, 1.0);
+		
+		int k = 20;
+		for (int i = 0; i < k; i++)
+		{
+			// Authority
+			//cout << "--------------Auth-----------------" << endl;
+			node p;
+			forall_nodes (p, G)
+			{
+				//cout << G.get_node_label(p) << ":";
+				
+				double sum = 0.0;
+				
+				node::in_edges_iterator it = p.in_edges_begin();
+				node::in_edges_iterator end = p.in_edges_end();
+				while (it != end)
+				{
+					node q = p.opposite(*it);
+					
+					sum += hub_weight[q];
+					
+					//cout << q << " ";
+					it++;
+				}
+				//cout << endl;
+				
+				authority_weight[p] = sum;
+			}
+			
+			// Normalise
+			double total = 0.0;
+			forall_nodes (p, G)
+			{
+				total += authority_weight[p] * authority_weight[p];
+			}
+			double sq = sqrt(total);
+			forall_nodes (p, G)
+			{
+				authority_weight[p] = authority_weight[p]/sq;
+			}
+			
+	
+			// Hub
+			//cout << "--------------Hub-----------------" << endl;
+			forall_nodes (p, G)
+			{
+				//cout << G.get_node_label(p) << ":";
+				
+				double sum = 0.0;
+				
+				node::out_edges_iterator it = p.out_edges_begin();
+				node::out_edges_iterator end = p.out_edges_end();
+				while (it != end)
+				{
+					node q = p.opposite(*it);
+					
+					sum += authority_weight[q];
+					
+					//cout << q << " ";
+					it++;
+				}
+				//cout << endl;
+				
+				hub_weight[p] = sum;
+			}
+	
+			// Normalise
+			total = 0.0;
+			forall_nodes (p, G)
+			{
+				total += hub_weight[p] * hub_weight[p];
+			}
+			sq = sqrt(total);
+			forall_nodes (p, G)
+			{
+				hub_weight[p] = hub_weight[p]/sq;
+			}
+	
+			
+		}
+		
+		// Results
+		cout << "Node\tAuthority\tHub" << endl;
+		node n;
+		forall_nodes (n, G)
+		{
+		
+		
+		
+			cout << G.get_node_label(n) 
+				<< "\t" << std::fixed << std::setprecision(3) << authority_weight[n] 
+				<< "\t" << std::fixed << std::setprecision(3) << hub_weight[n] 
+				<< endl;
+		}
+	}
+	
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,93 @@
-# Page_Rank-HITS_Computation
+
+
+# An Open Source PageRank Implementation
+
+This project provides an open source PageRank implementation. The
+implementation is a straightforward application of the algorithm
+description given in the American Mathematical Society's Feature
+Column.
+
+# Building
+
+The project is written in standard C++ and can be built by running:
+
+    g++ -o pagerank pagerank.cpp table.cpp table.h
+
+# Usage
+
+pagerank is invoked by
+
+    pagerank [OPTIONS] graph_file
+
+where OPTIONS may be:
+
+* -t: if set, tracing is enabled. Tracing outputs all intermediate
+   steps of the pagerank calculation algorithm and can be very
+   voluminous.
+
+* -n: if set, the graph_file is in numeric format, that is it consists
+   of lines of the form `<from><delim><to>` where `<from>` and `<to>` are
+   integer vertex indices, starting with zero. If not set, the
+   graph_file consists of lines of the form `<from><delim><to>` where
+   `<from>` and `<to>` are vertex IDs that will be interpreted as strings.
+
+* -a `<float>`: the pagerank dumping factor; default is  0.85.
+
+* -c `<float>`: the convergence criterion. The pagerank iterations will
+   stop when two successive iterations have converged to less than or
+   equal to this value. Default is 0.00001.
+
+* -s `<integer>`: the number of rows of the hyperlink matrix. This is
+   not the maximum size; if the graph requires more rows, they will be
+   allocated as necessary. If an approximate size is known beforehand,
+   however, the necessary internal data structures can be
+   pre-allocated for better performance.
+
+* -d `<string>`: the delimited used to separate vector indices in the
+   input graph file. Default is `" => "`.
+
+
+
+The test driver is written in standard C++ and can be compiled with:
+
+    g++ -I../cpp -o pagerank_test pagerank_test.cpp ../cpp/table.cpp 
+
+
+# C++ implementation of hubs and authorities (HITS) and PageRank algorithms
+
+
+#### Building
+If you are building from this repository you will need to do the standard things:
+
+* aclocal
+* autoconf
+* automake
+* ./configure
+* make
+
+#### Example
+
+The examples folder contains citation graph for an ant specimen and various entities (DNA sequences, images, publications) that "cite" that specimen:
+
+
+
+Running the program on this graph
+
+* matching examples/CASENT0106070.gml
+
+produces a text dump of PageRank values.
+
+	Graph read from file "examples/CASENT0106070.gml" has 10 nodes and 20 edges
+	Node	Page rank
+	CASENT0106070	1.25722
+	AY703487	0.194147
+	AY703554 	0.194147
+	AY703621 	0.194147
+	AY703688	0.194147
+	AY703755 	0.194147
+	EF013219	0.165938
+	EF013381	0.165938
+	doi:10.1111/j.1365-3113.2004.00281.x	0.165938
+	pmdi:17079492	0.15
+
+
